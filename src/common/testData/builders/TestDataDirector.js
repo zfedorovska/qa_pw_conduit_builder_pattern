abaa1120cd03839c3c1e2c78580108e47075b0ff
Zoryana Fedorovska <zfedorovska@gmail.com>
2025-09-22 00:01:44 +0300

    refactoring changes

--- src/common/testData/builders/TestDataDirector.js
+++ src/common/testData/builders/TestDataDirector.js
@@ -1,6 +1,6 @@
 import { UserDirector } from './user/UserDirector';
 import { ProfileDirector } from './profile/ProfileDirector';
-import { ArticleDirector } from './article/ArticleDirector';
+import { ArticleDirector } from './artcile/ArticleDirector';
 
 
 export class TestDataDirector {
--- tests/_fixtures/fixturesArticlesApi.ts
+++ tests/_fixtures/fixturesArticlesApi.ts
@@ -1,24 +1,32 @@
 import { test as base } from '@playwright/test';
 import { ArticlesApi } from '../../src/api/endpoints/ArticlesApi';
 
+type ArticlePayload = {
+  title: string;
+  description: string;
+  body: string;
+  tagList?: string[];
+};
+
 export const test = base.extend<{
-  articlesApi;
-  articleWithoutTags;
-  articleWithOneTag;
+  articlesApi: ArticlesApi;
+  articleWithoutTags: ArticlePayload;
+  articleWithOneTag: ArticlePayload;
 }>({
   articlesApi: async ({ request }, use) => {
     const client = new ArticlesApi(request);
-
     await use(client);
   },
-  articleWithoutTags: async ({}, use) => {
-    const article = generateNewArticleData();
 
+  // No tags
+  articleWithoutTags: async ({}, use) => {
+    const article = testDataDirector.article().withTags(0).build();
     await use(article);
   },
-  articleWithOneTag: async ({}, use) => {
-    const article = generateNewArticleData(1);
 
+  // Exactly one tag
+  articleWithOneTag: async ({}, use) => {
+    const article = testDataDirector.article().withTags(1).build();
     await use(article);
   },
 });
